6d55681733c024a9c10fc0bcfa3c1c7319cabe2f
XyL <y917059177y@gmail.com>
2016-04-30 18:14:58 +0800

    更新Crime，添加日期、是否解决及Getter和Setter

--- app/src/main/java/com/example/administrator/crimeintent/Crime.java
+++ app/src/main/java/com/example/administrator/crimeintent/Crime.java
@@ -1,18 +1,22 @@
 package com.example.administrator.crimeintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
  * Created by Administrator on 2016/4/30.
+ * 保存每个Crime的Id，标题，日期，是否解决
  */
 public class Crime {
 
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSloved;
 
     public Crime() {
         mId = UUID.randomUUID();
-
+        mDate = new Date();
     }
 
     public UUID getId() {
@@ -25,5 +29,21 @@ public class Crime {
 
     public void setTitle(String title) {
         mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSloved() {
+        return mSloved;
+    }
+
+    public void setSloved(boolean sloved) {
+        mSloved = sloved;
     }
 }
