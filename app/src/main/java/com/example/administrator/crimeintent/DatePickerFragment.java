50b442430af208852883066644ea8be4d7f1c812
y917059177y <x7985959683y>
2016-05-08 13:12:27 +0800

    把AlterDialog设置为DatePicker

--- app/src/main/java/com/example/administrator/crimeintent/DatePickerFragment.java
+++ app/src/main/java/com/example/administrator/crimeintent/DatePickerFragment.java
@@ -4,6 +4,8 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
 
 /**
  * Created by XyL on 2016/5/8.
@@ -13,7 +15,11 @@ public class DatePickerFragment extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        View v = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_date, null);
+
         return new AlertDialog.Builder(getActivity())
+                .setView(v)
                 .setTitle(R.string.date_picker_title)
                 .setPositiveButton(android.R.string.ok, null)
                 .create();
